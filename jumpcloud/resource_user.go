2d59ab550dc37ecc1226203915daaa361c6acd97
daniellohausen <daniel.lohausen@cognotekt.com>
2018-12-21 10:19:20 +0100

    Added UD (from CRUD) and additional parameters

--- jumpcloud/resource_user.go
+++ jumpcloud/resource_user.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	//    "net/http"
 	"fmt"
+
 	jcapiv1 "github.com/TheJumpCloud/jcapi-go/v1"
 	jcapiv2 "github.com/TheJumpCloud/jcapi-go/v2"
 	"github.com/hashicorp/terraform/helper/schema"
@@ -19,6 +20,7 @@ func resourceUser() *schema.Resource {
 			"username": {
 				Type:     schema.TypeString,
 				Required: true,
+				ForceNew: true,
 			},
 			"xorgid": {
 				Type:     schema.TypeString,
@@ -26,6 +28,18 @@ func resourceUser() *schema.Resource {
 			},
 			"email": {
 				Type:     schema.TypeString,
+				Required: true,
+			},
+			"firstname": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"lastname": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"enable_mfa": {
+				Type:     schema.TypeBool,
 				Optional: true,
 			},
 		},
@@ -45,6 +59,9 @@ func resourceUserCreate(d *schema.ResourceData, m interface{}) error {
 	var payload jcapiv1.Systemuserputpost
 	payload.Username = d.Get("username").(string)
 	payload.Email = d.Get("email").(string)
+	payload.Firstname = d.Get("firstname").(string)
+	payload.Lastname = d.Get("lastname").(string)
+	payload.EnableUserPortalMultifactor = d.Get("enable_mfa").(bool)
 
 	req := map[string]interface{}{
 		"body":   payload,
@@ -70,7 +87,6 @@ func resourceUserRead(d *schema.ResourceData, m interface{}) error {
 		d.Id(), "", "", nil)
 
 	if err != nil {
-		fmt.Errorf("Error Getting user info")
 		return err
 	}
 
@@ -90,12 +106,42 @@ func resourceUserRead(d *schema.ResourceData, m interface{}) error {
 	if err := d.Set("email", res.Email); err != nil {
 		return err
 	}
-	return nil
+	if err := d.Set("firstname", res.Firstname); err != nil {
+		return err
+	}
+	if err := d.Set("lastname", res.Lastname); err != nil {
+		return err
+	}
 
+	if err := d.Set("enable_mfa", res.EnableUserPortalMultifactor); err != nil {
+		return err
+	}
+	return nil
 }
 
 func resourceUserUpdate(d *schema.ResourceData, m interface{}) error {
+	configv2 := m.(*jcapiv2.Configuration)
+	configv1 := jcapiv1.NewConfiguration()
+	configv1.AddDefaultHeader("x-api-key", configv2.DefaultHeader["x-api-key"])
 
+	client := jcapiv1.NewAPIClient(configv1)
+
+	var payload jcapiv1.Systemuserput
+	payload.Username = d.Get("username").(string)
+	payload.Email = d.Get("email").(string)
+	payload.Firstname = d.Get("firstname").(string)
+	payload.Lastname = d.Get("lastname").(string)
+	payload.EnableUserPortalMultifactor = d.Get("enable_mfa").(bool)
+
+	req := map[string]interface{}{
+		"body":   payload,
+		"xOrgId": d.Get("xorgid").(string),
+	}
+	_, _, err := client.SystemusersApi.SystemusersPut(context.TODO(),
+		d.Id(), "", "", req)
+	if err != nil {
+		return err
+	}
 	return resourceUserRead(d, m)
 }
 
