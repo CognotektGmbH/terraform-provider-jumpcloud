9f3d3449d293952fcc897640208cf729ad9e6146
daniellohausen <daniel.lohausen@cognotekt.com>
2019-01-08 13:42:20 +0100

    Added function for acceptance tests

--- jumpcloud/provider_test.go
+++ jumpcloud/provider_test.go
@@ -3,8 +3,20 @@ package jumpcloud
 // see https://www.terraform.io/docs/plugins/provider.html#provider
 
 import (
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
 	"testing"
 )
+
+var testAccProviders map[string]terraform.ResourceProvider
+var testAccProvider *schema.Provider
+
+func init() {
+	testAccProvider = Provider()
+	testAccProviders = map[string]terraform.ResourceProvider{
+		"jumpcloud": testAccProvider,
+	}
+}
 
 func TestProvider(t *testing.T) {
 	if err := Provider().InternalValidate(); err != nil {
