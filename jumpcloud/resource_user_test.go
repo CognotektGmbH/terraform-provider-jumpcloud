feda0e297e64e6fbb85539558001ac90d65c2b60
daniellohausen <daniel.lohausen@cognotekt.com>
2019-01-08 13:42:51 +0100

    Acceptance test for users

---
+++ jumpcloud/resource_user_test.go
@@ -0,0 +1,54 @@
+package jumpcloud
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+	"os"
+	"testing"
+)
+
+func TestAccUser(t *testing.T) {
+	rName := acctest.RandStringFromCharSet(10, acctest.CharSetAlpha)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: nil,
+		Steps: []resource.TestStep{
+			{
+				// This test simply aplys a user with the config from testAccUser
+				// and checks for the correct username and email in the state
+				// The resource is destroyed afterwards via the framework
+				Config: testAccUser(rName),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("jumpcloud_user.test_user", "username", rName),
+					resource.TestCheckResourceAttr("jumpcloud_user.test_user", "email", rName+"@testorg.com"),
+				),
+			},
+		},
+	})
+}
+
+// testAccPreCheck validates the necessary test API keys exist
+// in the testing environment
+func testAccPreCheck(t *testing.T) {
+	if v := os.Getenv("JUMPCLOUD_API_KEY"); v == "" {
+		t.Fatal("JUMPCLOUD_API_KEY= must be set for acceptance tests")
+	}
+}
+
+func testAccUser(name string) string {
+	return fmt.Sprintf(`
+
+	resource "jumpcloud_user" "test_user" {
+  username = "%s"
+  email = "%s@testorg.com"
+  firstname = "Firstname"
+  lastname = "Lastname"
+  enable_mfa = true
+  }
+`,
+		name, name,
+	)
+}
