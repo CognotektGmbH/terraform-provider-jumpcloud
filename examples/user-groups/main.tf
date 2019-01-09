f0be027e0eb9f8b4a447e2f5dbc84076c1684e2b
daniellohausen <daniel.lohausen@cognotekt.com>
2019-01-09 09:24:22 +0100

    Added users to example

--- examples/user-groups/main.tf
+++ examples/user-groups/main.tf
@@ -1,15 +1,22 @@
-resource "jumpcloud_user_group" "language" {
-    name = "language"
-    attributes {}
-}
-
-resource "jumpcloud_user_group" "cars" {
-    name = "cars"
-}
-
-resource "jumpcloud_user_group" "foo" {
-    name = "barz"
+resource "jumpcloud_user_group" "test_group" {
+    name = "test_group"
     attributes {
       posix_groups = "32:testerino"
     }
+}
+
+resource "jumpcloud_user" "test_user1" {
+    username = "testuser1"
+    firstname = "test"
+    lastname = "user1"
+    email = "testuser1@testorg.org"
+    enable_mfa = true
+}
+
+resource "jumpcloud_user" "test_user2" {
+    username = "testuser2"
+    firstname = "test"
+    lastname = "user2"
+    email = "testuser2@testorg.org"
+    enable_mfa = false
 }
