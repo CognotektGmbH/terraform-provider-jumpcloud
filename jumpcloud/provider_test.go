c62d67a2c267f7b75d58e814f1aeb9fb1de07b92
David Bolack <dbolack@enthought.com>
2020-05-06 16:32:51 -0500

    Update code to use terraform-plugin-sdk instead of terraform modules.

--- jumpcloud/provider_test.go
+++ jumpcloud/provider_test.go
@@ -5,8 +5,8 @@ package jumpcloud
 import (
 	"testing"
 
-	"github.com/hashicorp/terraform/helper/schema"
-	"github.com/hashicorp/terraform/terraform"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
 var (
