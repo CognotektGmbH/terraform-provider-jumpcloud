df3de28006567cf519289fe7315ce6da8f477d80
cheelim1 <ctoh@moneylion.com>
2023-04-20 13:49:08 +0800

    update examples

---
+++ examples/user-groups/data.tf
@@ -0,0 +1,9 @@
+# jumpcloud_user Data Source
+
+## Use this data source to get the ID of a JumpCloud user by their email address.
+
+## Example Usage
+
+data "jumpcloud_user" "example" {
+  email = "user@example.com"
+}
